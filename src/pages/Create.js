5ea7579ed5417c3aae6aad7ac1a6f3ef3e549159
Monzer Abdullaziz <nezonru87@yahoo.com>
2021-06-17 16:21:39 +0200

    tell buttons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import {Typography} from '@material-ui/core'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
+    <Container>
+      <Typography
+        variant="h6" 
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+    <Button type="submit"
+     color="secondary"
+     variant="contained"
+      onClick={()=> console.log("you clicked me")}
+    >
+      submit
+    </Button>
+    </Container>
   )
 }
